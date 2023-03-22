9bb4d60a1fa836503d192c996390b8440b540ff3
Brian May <brian@linuxpenguins.xyz>
2023-03-22 21:15:22 +1100

    Initial version

---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  description = "zoiper5 package";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+
+  outputs = { self, nixpkgs }: {
+    packages.x86_64-linux.default =
+      with import nixpkgs { system = "x86_64-linux"; };
+      let
+        src = fetchurl {
+          url = "https://shop.zoiper.com/download.php?id=OLPBMFJT3SD5PM3";
+          sha256 = "sha256-jTw0UxS9JiCJmFWndOOP9Wz2A69k2cN1C925uCUMvuM=";
+        };
+      in stdenv.mkDerivation {
+        pname = "zoiper5";
+        version = "21.07.0";
+
+        src = src;
+
+        nativeBuildInputs = [ autoPatchelfHook ];
+
+        buildInputs = [
+          alsa-lib
+          at-spi2-core
+          cairo
+          cups
+          dbus
+          expat
+          gdk-pixbuf
+          gnome2.libgnome
+          krb5
+          libdrm
+          libnotify
+          libpulseaudio
+          libv4l
+          libxkbcommon
+          mesa
+          nss
+          pango
+          xorg.libXcomposite
+          xorg.libXdamage
+          xorg.libXrandr
+          xorg.libXScrnSaver
+        ];
+
+        sourceRoot = ".";
+
+        unpackPhase = ":";
+
+        installPhase = ''
+          mkdir -p "$out/opt"
+          mkdir -p "$out/bin"
+          tar -C "$out/opt" -xvf ${src}
+          ln -s ../opt/Zoiper5/zoiper "$out/bin"
+        '';
+
+        meta = with lib; {
+          homepage = "https://www.zoiper.com/";
+          description = "Voip softphone";
+          platforms = platforms.linux;
+          architectures = [ "x86" ];
+        };
+      };
+  };
+
+}
