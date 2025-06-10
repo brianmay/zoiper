dd24f0aab70aed926e50e83718767d1b55767a4a
Brian May <brian@linuxpenguins.xyz>
2025-06-11 06:58:32 +1000

    fix: update for zoiper 5.6.9

--- flake.nix
+++ flake.nix
@@ -1,66 +1,71 @@
 {
   description = "zoiper5 package";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: {
-    packages.x86_64-linux.default = with import nixpkgs {system = "x86_64-linux";}; let
-      src = ./Zoiper5_5.6.4_x86_64.tar.xz;
-    in
-      stdenv.mkDerivation {
-        pname = "zoiper5";
-        version = "5.6.4";
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    {
+      packages.x86_64-linux.default =
+        with import nixpkgs { system = "x86_64-linux"; };
+        let
+          src = ./Zoiper5_5.6.9_x86_64.tar.xz;
+        in
+        stdenv.mkDerivation {
+          pname = "zoiper5";
+          version = "5.6.9";
 
-        src = src;
+          src = src;
 
-        nativeBuildInputs = [autoPatchelfHook];
+          nativeBuildInputs = [ autoPatchelfHook ];
 
-        buildInputs = [
-          alsa-lib
-          at-spi2-core
-          cairo
-          cups
-          dbus
-          expat
-          gdk-pixbuf
-          gnome2.libgnome
-          krb5
-          libdrm
-          libnotify
-          libpulseaudio
-          libv4l
-          libxkbcommon
-          mesa
-          nss
-          pango
-          xorg.libXcomposite
-          xorg.libXdamage
-          xorg.libXrandr
-          xorg.libXScrnSaver
-          libva
-          pciutils
-        ];
+          buildInputs = [
+            alsa-lib
+            at-spi2-core
+            cairo
+            cups
+            dbus
+            expat
+            gdk-pixbuf
+            libGL
+            gtk2
+            krb5
+            libdrm
+            libnotify
+            libpulseaudio
+            libv4l
+            libxkbcommon
+            mesa
+            nss
+            pango
+            xorg.libXcomposite
+            xorg.libXdamage
+            xorg.libXrandr
+            xorg.libXScrnSaver
+            libva
+            pciutils
+          ];
 
-        sourceRoot = ".";
+          sourceRoot = ".";
 
-        unpackPhase = ":";
+          unpackPhase = ":";
 
-        installPhase = ''
-          mkdir -p "$out/opt"
-          mkdir -p "$out/bin"
-          tar -C "$out/opt" -xvf ${src}
-          ln -s ../opt/Zoiper5/zoiper "$out/bin"
-        '';
+          installPhase = ''
+            mkdir -p "$out/opt"
+            mkdir -p "$out/bin"
+            tar -C "$out/opt" -xvf ${src}
+            ln -s ../opt/Zoiper5/zoiper "$out/bin"
+          '';
 
-        meta = with lib; {
-          description = "Voip softphone";
-          homepage = "https://www.zoiper.com/";
-          # license = licenses.unfree;
-          platforms = platforms.linux;
-          architectures = ["x86"];
+          meta = with lib; {
+            description = "Voip softphone";
+            homepage = "https://www.zoiper.com/";
+            # license = licenses.unfree;
+            platforms = platforms.linux;
+            architectures = [ "x86" ];
+          };
         };
-      };
-  };
+    };
 }
